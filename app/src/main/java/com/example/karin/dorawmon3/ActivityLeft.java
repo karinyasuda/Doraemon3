55ab83370f2ebb9d4379a6629e1f50feeb46b25b
Karin <karin.03k35@gmail.com>
2016-01-23 20:12:00 +0900

    test commit

--- app/src/main/java/com/example/karin/dorawmon3/ActivityLeft.java
+++ app/src/main/java/com/example/karin/dorawmon3/ActivityLeft.java
@@ -12,6 +12,7 @@ public class ActivityLeft extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_left);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
